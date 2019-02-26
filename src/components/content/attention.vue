1fcc8130076154c35375bfa2169bf4a3413d3d05
git478345960 <478345960@qq.com>
2019-02-26 21:27:41 +0800

    第一页面基本完成

--- src/components/content/attention.vue
+++ src/components/content/attention.vue
@@ -1,14 +1,96 @@
 <template>
-  <div>注意事项</div>
-
+  <div class="experienceWrap">
+    <div class="logo">
+      <h2>注意事项</h2>
+      <h3>让注意力更集中</h3>
+    </div>
+    <div class="experienceContent">
+      <ul>
+        <li><h1>1.不要轻易相信没投递简历的公司</h1><p>存在一些培训公司，拉人去他公司培训，没有工资还要倒贴一些给他，不要被他的花言巧语所欺骗，他们所说的很多东西都不会真正实现</p></li>
+        <li><h1>1.不要轻易相信没投递简历的公司</h1><p>存在一些培训公司，拉人去他公司培训，没有工资还要倒贴一些给他，不要被他的花言巧语所欺骗，他们所说的很多东西都不会真正实现</p></li>
+        <li><h1>1.不要轻易相信没投递简历的公司</h1><p>存在一些培训公司，拉人去他公司培训，没有工资还要倒贴一些给他，不要被他的花言巧语所欺骗，他们所说的很多东西都不会真正实现</p></li>
+        <li><h1>1.不要轻易相信没投递简历的公司</h1><p>存在一些培训公司，拉人去他公司培训，没有工资还要倒贴一些给他，不要被他的花言巧语所欺骗，他们所说的很多东西都不会真正实现</p></li>
+        <li><h1>1.不要轻易相信没投递简历的公司</h1><p>存在一些培训公司，拉人去他公司培训，没有工资还要倒贴一些给他，不要被他的花言巧语所欺骗，他们所说的很多东西都不会真正实现</p></li>
+        <li><h1>1.不要轻易相信没投递简历的公司</h1><p>存在一些培训公司，拉人去他公司培训，没有工资还要倒贴一些给他，不要被他的花言巧语所欺骗，他们所说的很多东西都不会真正实现</p></li>
+        <li><h1>1.不要轻易相信没投递简历的公司</h1><p>存在一些培训公司，拉人去他公司培训，没有工资还要倒贴一些给他，不要被他的花言巧语所欺骗，他们所说的很多东西都不会真正实现</p></li>
+        <li><h1>1.不要轻易相信没投递简历的公司</h1><p>存在一些培训公司，拉人去他公司培训，没有工资还要倒贴一些给他，不要被他的花言巧语所欺骗，他们所说的很多东西都不会真正实现</p></li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script>
-export default{
+export default {
+  data(){
+    return {
 
-}
+    }
+  }
+};
 </script>
 
-<style scoped>
+<style lang = "scss" scoped>
+.experienceWrap {
+  width: 1300px;
+  height: 650px;
+  margin: 0 auto;
+  .logo {
+    min-width: 1024px;
+    height: 125px;
+    padding-top: 35px;
+    background: #100f06 url(//www.lgstatic.com/www/static/community/modules/index/banner/img/banner-bg_21a3371.jpg) center center no-repeat;
 
+    h2 {
+      margin:0 auto;
+      margin-bottom: 6px;
+      font-size: 30px;
+      font-weight: 700;
+      line-height: 40px;
+      color: #fff;
+      text-align: center;
+    }
+    h3 {
+      margin-bottom: 4px;
+      font-size: 26px;
+      line-height: 36px;
+      color: #fff;
+      text-align: center;
+    }
+  }
+  .experienceContent{
+    height:490px;
+    ul{
+      li{
+        float:left;
+        width:627px;
+        height:100px;
+        padding:10px;
+        margin-bottom:2px;
+        margin-right:2px;
+        /* border-bottom:1px dashed grey; */
+        border-right:1px dashed grey;       
+          h1{
+            padding:5px;
+            color:#333;
+            padding:5px;
+            &:hover{
+              color: #00b38a;
+            }
+            
+          }
+          p{
+            max-height: 88px;
+            color:#555;
+            padding:10px;
+            overflow:hidden;
+            text-overflow:ellipsis;
+            /* white-space: nowrap; */
+          }
+          
+        
+        
+      }
+    }
+  }
+}
 </style>
--- src/components/content/experience.vue
+++ src/components/content/experience.vue
@@ -1,14 +1,87 @@
 <template>
-  <div>心得页面</div>
-
+  <div class="experienceWrap">
+    <div class="logo">
+      <h2>听取经验</h2>
+      <h3>让自己少走弯路</h3>
+    </div>
+    <div class="experienceContent">
+      <ul>
+        <li><a href="#"><h1>title</h1><p>contentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontent</p></a></li>
+        <li><a href="#"><h1>title</h1><p>contentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontent</p></a></li>
+        <li><a href="#"><h1>title</h1><p>contentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontent</p></a></li>
+        <li><a href="#"><h1>title</h1><p>contentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontent</p></a></li>
+        <li><a href="#"><h1>title</h1><p>contentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontent</p></a></li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script>
-export default{
-
-}
+export default {};
 </script>
 
-<style scoped>
+<style lang = "scss" scoped>
+.experienceWrap {
+  width: 1300px;
+  height: 650px;
+  margin: 0 auto;
+  .logo {
+        min-width: 1024px;
+    height: 125px;
+    padding-top: 35px;
+    background: #100f06 url(//www.lgstatic.com/www/static/community/modules/index/banner/img/banner-bg_21a3371.jpg) center center no-repeat;
 
+    h2 {
+      margin:0 auto;
+      margin-bottom: 6px;
+      font-size: 30px;
+      font-weight: 700;
+      line-height: 40px;
+      color: #fff;
+      text-align: center;
+    }
+    h3 {
+      margin-bottom: 4px;
+      font-size: 26px;
+      line-height: 36px;
+      color: #fff;
+      text-align: center;
+    }
+  }
+  .experienceContent{
+    height:490px;
+    ul{
+      li{
+        float:left;
+        width:627px;
+        height:80px;
+        padding:10px;
+        margin-bottom:2px;
+        margin-right:2px;
+        /* border-bottom:1px dashed grey; */
+        border-right:1px dashed grey;
+        a{
+          text-decoration: none;
+          
+          h1{
+            padding:5px;
+            color:#333;
+            &:hover{
+              color: #00b38a;
+            }
+          }
+          p{
+            color:#555;
+            padding:10px;
+            overflow:hidden;
+            text-overflow:ellipsis;
+            white-space: nowrap;
+          }
+          
+        }
+        
+      }
+    }
+  }
+}
 </style>
